8279f85f41fdf03aba464558a477da5cab38ba0d
PRAVEEN AGRAWAL <praveenagrawal@PRAVEENs-MacBook-Air.local>
2026-03-12 01:00:54 +0530

    first commit

---
+++ src/main/java/com/git/Controller/UserController.java
@@ -0,0 +1,18 @@
+package com.git.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    GetMapping("/demo")
+    public String getUser(){
+        return "Hello User";
+    }
+
+}
+
+
